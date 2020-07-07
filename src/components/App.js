15f2e0a75cc8acf09c94151e8dd957dd8a0cc5f4
Jorge Garcia <jagu.ittis@gmail.com>
2020-07-07 17:04:40 +0200

    Include new Components in Browser Component inside App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList}></Route>
+          <Route path="/streams/new" component={StreamCreate}></Route>
+          <Route path="/streams/edit" component={StreamEdit}></Route>
+          <Route path="/streams/delete" component={StreamDelete}></Route>
+          <Route path="/streams/show" component={StreamShow}></Route>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;

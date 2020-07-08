96adbbb91b2fcff59553a8d567ca0288919c3f5f
Jorge Garcia <jagu.ittis@gmail.com>
2020-07-08 09:47:56 +0200

    Add some buttons and actions to Sign In or Sign Out
    All actions are done using Google Auth API

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -23,13 +23,31 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I do not know of we are signed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>;
+      return (
+        <button className="ui red google button" onClick={this.onSignOut}>
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>I am NOT signed in</div>;
+      return (
+        <button className="ui red google button" onClick={this.onSignIn}>
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
 

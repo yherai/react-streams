d0cb69e5b6900ab9f9581494716aafe7ac68b5be
Jorge Garcia <jagu.ittis@gmail.com>
2020-07-15 16:50:24 +0200

    Added delete action when the user deletes a stream and navigates back to main component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Modal from '../Modal';
 import history from '../../history';
-import { fetchStream } from '../../actions';
+import { fetchStream, deleteStream } from '../../actions';
 
 class StreamDelete extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
   renderActions() {
+    const { id } = this.props.match.params;
     return (
       <React.Fragment>
-        <button className="ui negative button">Delete</button>
-        <button className="ui button">Cancel</button>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui negative button"
+        >
+          Delete
+        </button>
+        <Link className="ui button" to="/">
+          Cancel
+        </Link>
       </React.Fragment>
     );
   }
+
   renderContent() {
     if (!this.props.stream) {
       return 'Are you sure you want to delete this stream?';
@@ -39,4 +50,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamDelete);
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);

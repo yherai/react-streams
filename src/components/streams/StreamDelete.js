fe6ba384ca52762833941897ebaec4d9f6c91540
Jorge Garcia <jagu.ittis@gmail.com>
2020-07-15 16:25:57 +0200

    Make Modal component customizable
    Renamed file modal.js to Modal.js for convention

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Modal from '../modal';
+import Modal from '../Modal';
 
 const StreamDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui negative button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
+
   return (
     <div>
       StreamDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
     </div>
   );
 };

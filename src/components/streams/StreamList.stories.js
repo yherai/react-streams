e6266f7a03a8404c0d7f49f00ab678d99ad4064b
Jorge Garcia <jagu.ittis@gmail.com>
2020-08-03 16:27:39 +0200

    Update StreamList story to have only default story
    This was made as StreamList is listening the changes in the store.
    
    The sigIn values are updated only in App component stories.
    
    Remove not needed conslo.log from StreamList component

--- src/components/streams/StreamList.stories.js
+++ src/components/streams/StreamList.stories.js
@@ -4,56 +4,10 @@ import { storiesOf } from '@storybook/react';
 import { Router } from 'react-router-dom';
 import history from '../../history';
 
-const streams = [
-  {
-    title: 'My Stream',
-    description: 'This is a great stream!',
-    id: 1,
-  },
-  {
-    title: 'I created this stream',
-    description: 'The stream is about refactoring',
-    id: 2,
-  },
-  {
-    title: 'Me streaming component reuse',
-    description: 'Me refactoring components as an example',
-    id: 3,
-  },
-  {
-    title: 'This another example',
-    description: 'Now buttons should be shown!',
-    userId: '102267598368443841755',
-    id: 4,
-  },
-];
-
-const notSignedState = {
-  auth: {
-    isSignedIn: false,
-  },
-  streams,
-};
-const signedState = {
-  auth: {
-    isSignedIn: true,
-    userId: '102267598368443841755',
-  },
-  streams,
-};
-
-storiesOf('StreamList', module)
-  .add('Not signed In', () => (
-    <div className="ui container">
-      <Router history={history}>
-        <StreamList state={notSignedState} />
-      </Router>
-    </div>
-  ))
-  .add('Signed In', () => (
-    <div className="ui container">
-      <Router history={history}>
-        <StreamList state={signedState} />
-      </Router>
-    </div>
-  ));
+storiesOf('StreamList', module).add('Default', () => (
+  <div className="ui container">
+    <Router history={history}>
+      <StreamList />
+    </Router>
+  </div>
+));
